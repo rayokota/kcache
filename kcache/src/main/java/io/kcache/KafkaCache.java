ee50a9cb02160aa9cf8ea5d84e59b70608b9631a
Robert Yokota <rayokota@gmail.com>
2020-10-02 19:46:12 -0700

    Minor optimization

--- kcache/src/main/java/io/kcache/KafkaCache.java
+++ kcache/src/main/java/io/kcache/KafkaCache.java
@@ -781,7 +781,7 @@ public class KafkaCache<K, V> implements Cache<K, V> {
                 }
                 count = records.count();
                 cacheUpdateHandler.checkpoint(count);
-                if (localCache.isPersistent()) {
+                if (localCache.isPersistent() && initialized.get()) {
                     try {
                         localCache.flush();
                         checkpointOffsets();
